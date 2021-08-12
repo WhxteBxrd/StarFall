442dc9b5b685053de779e7a6629db7736f7f6097
WhxteBxrd <whxtebxrd0141650@gmail.com>
2021-08-12 18:09:49 +0300

    first commit

---
+++ Start_Form.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Starfall_Screensaver_Win98_
+{
+    partial class Start_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._MyTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _MyTextBox
+            // 
+            this._MyTextBox.Location = new System.Drawing.Point(125, 87);
+            this._MyTextBox.Name = "_MyTextBox";
+            this._MyTextBox.Size = new System.Drawing.Size(206, 20);
+            this._MyTextBox.TabIndex = 0;
+            this._MyTextBox.Text = "30";
+            this._MyTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this._MyTextBox_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(151, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(151, 59);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.MediumTurquoise;
+            this.label1.Location = new System.Drawing.Point(165, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "STARS SPEED";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::Starfall_Screensaver_Win98_.Properties.Resources._97dd604f4c71a4093851ad6afbbe0f3b1;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(455, 314);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Start_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(455, 314);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this._MyTextBox);
+            this.Controls.Add(this.pictureBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(471, 353);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(471, 353);
+            this.Name = "Start_Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Start_Form";
+            this.Load += new System.EventHandler(this.Start_Form_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox _MyTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Start_Form.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Starfall_Screensaver_Win98_
+{
+    public partial class Start_Form : Form
+    {
+        public Start_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Start_Form_Load(object sender, EventArgs e)
+        {
+            label1.Parent = pictureBox1;
+            label1.BackColor = Color.Transparent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            Form1 form1 = new Form1();
+
+            form1.PeredanSpeed = this._MyTextBox.Text;
+
+            form1.Show();            
+        }
+
+        //Подписка на TextBox, событие KeyPress 
+        private void _MyTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char ch = e.KeyChar; //Для хранения нажатой клаваши
+
+            //Если нажата клав. с буквой, ввод запрещён
+            //Искл. только для Backspace!!!
+            if(!Char.IsDigit(ch) && ch != 8)
+            {
+                e.Handled = true; 
+            }
+        }
+    }
+}
